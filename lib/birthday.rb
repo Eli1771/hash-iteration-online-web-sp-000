dbe251bfab9d4051a4ca292f0783fe1a833d8eff
elijahmm17 <elijahmm17@gmail.com>
2019-05-16 20:51:12 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,7 +6,11 @@
 
 def happy_birthday(birthday_kids)
   birthday_kids.each do |name, age|
-    puts "Happy Birthday #{name}! You are now #{age} years old!"
+    if age < 13
+      puts "Happy Birthday #{name}! You are now #{age} years old!"
+    else 
+      puts "You are too old for this."
+    end
   end
 end
 
