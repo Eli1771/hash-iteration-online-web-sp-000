998dc267366d0179c83820270ccd6d1d22d61a89
elijahmm17 <elijahmm17@gmail.com>
2019-05-16 20:52:00 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -4,7 +4,7 @@
 #   "Amanda" => 27
 # }
 
-def happy_birthday(birthday_kids)
+def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |name, age|
     if age < 13
       puts "Happy Birthday #{name}! You are now #{age} years old!"
@@ -14,4 +14,12 @@ def happy_birthday(birthday_kids)
   end
 end
 
-
+def happy_birthday(birthday_kids)
+  birthday_kids.each do |name, age|
+    if age < 13
+      puts "Happy Birthday #{name}! You are now #{age} years old!"
+    else 
+      puts "You are too old for this."
+    end
+  end
+end
